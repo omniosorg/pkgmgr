cbc6acb1589bf68ada9cd260eeb0ae526f6acd3f
Dominik Hassler <hadfl@omniosce.org>
2017-09-08 01:14:23 +0200

    add minimal POD

--- lib/PkgMgr/Config.pm
+++ lib/PkgMgr/Config.pm
@@ -128,3 +128,33 @@ sub loadConfig {
 }
 
 1;
+
+__END__
+
+=head1 COPYRIGHT
+
+Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
+
+=head1 LICENSE
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see L<http://www.gnu.org/licenses/>.
+
+=head1 AUTHOR
+
+S<Andy Fiddaman E<lt>omnios@citrus-it.co.ukE<gt>>
+S<Dominik Hassler E<lt>hadfl@omniosce.orgE<gt>>
+
+=head1 HISTORY
+
+2017-09-06 had Initial Version
+
+=cut
